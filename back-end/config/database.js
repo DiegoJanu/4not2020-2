8592b54b8b77c5d62327813eaf0595d412955643
DiegoJanu <diegosjanuario@hotmail.com>
2020-09-01 01:22:12 +0000

    (31/08) Término da configuração da conexão de BD e primeiro model

--- back-end/config/database.js
+++ back-end/config/database.js
@@ -5,4 +5,22 @@ module.exports = uri => {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
+
+
+mongoose.connection.on('connected',() =>
+console.log('==> Mongoose! conectado com sucesso ao servidor')
+)
+
+mongoose.connection.on('disconnected', () =>
+    console.log('==>Mongoose! desconectado do servidor')
+)
+
+process.on('SIGINT' ,() =>
+    mongoose.connection.close(() => {
+        console.log('==> Mongoose! Desconectado pelo término da aplicação')
+        process.exit(0);
+
+    })
+)
+
 }
